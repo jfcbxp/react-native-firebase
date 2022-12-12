74e65908d07a05b3f02d506cc647ca512f37712b
Joao Felipe <jfcbxp@gmail.com>
2022-12-12 15:01:45 -0300

    configuracao inicial firebase

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { realtime } from "./src/services/firebaseConnection";
 
 export default function App() {
+  const [nome, setNome] = useState("Carregando");
+
+  useEffect(() => {
+    //obter em tempo real as alteracoes
+    /*
+    realtime.ref("nome").on("value", (snapshot) => {
+      setNome(snapshot.val());
+    });
+    */
+    realtime.ref("nome").once("value", (snapshot) => {
+      setNome(snapshot.val());
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{nome}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
